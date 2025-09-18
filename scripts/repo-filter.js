3cdeaa40b4548df8d32de8b7306dd58a0549adfb
parsehex <1779937+parsehex@users.noreply.github.com>
2025-09-18 12:13:11 -0400

    Update repo-filter.js

--- scripts/repo-filter.js
+++ scripts/repo-filter.js
@@ -6,8 +6,17 @@
  * @returns {Array} The filtered array of repositories
  */
 export function getFilteredRepos(data) {
-	// Default filter: keeps repos with a homepage
-	// If you want to filter repos by a naming convention, use e.g.:
+	// Default filter: keeps repos with a homepage URL
+
+	// ! Only 1 `return` line below should be active!
+
+	// == Filters ==
+	// naming convention - only repos starting with "portfolio-"
 	// return data.filter(repo => repo.name.startsWith('portfolio-'));
+
+	// repos with a homepage URL + a description
+	// return data.filter(repo => repo.homepage && repo.description);
+
+	// repos with a homepage URL
 	return data.filter((repo) => repo.homepage);
 }
