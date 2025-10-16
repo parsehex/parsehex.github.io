9f9165e6c6041a26952708defa1cb23ba46244c7
parsehex <1779937+parsehex@users.noreply.github.com>
2025-10-16 11:06:58 -0400

    handle footer text better
    spacing and also use it as (purified) html

--- src/Footer.vue
+++ src/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-	<footer class="mt-8 text-center text-gray-600 dark:text-gray-400">
-		<p v-if="footerText">{{ footerText }}</p>
+	<footer class="mt-8 space-y-2 text-center text-gray-600 dark:text-gray-400">
+		<p v-if="footerText" v-html="footerText"></p>
 		<p v-if="includeGitCaseLink" class="text-xs"> Showcase your GitHub repositories with <a
 				href="https://github.com/ProjectDepot/Gallery" target="_blank" rel="noopener noreferrer"
 				class="underline hover:text-blue-500"> Project Depot Gallery </a>
@@ -9,11 +9,12 @@
 </template>
 <script setup lang="ts">
 import { computed, toRefs } from 'vue'
+import DOMPurify from 'dompurify'
 import { useConfigStore } from './stores/config'
 
 const configStore = useConfigStore()
 const { config } = toRefs(configStore)
 
-const footerText = computed(() => config.value?.footer.text || '')
+const footerText = computed(() => config.value?.footer.text ? DOMPurify.sanitize(config.value?.footer.text) : '')
 const includeGitCaseLink = computed(() => config.value?.footer.includeGitCaseLink !== false)
 </script>
