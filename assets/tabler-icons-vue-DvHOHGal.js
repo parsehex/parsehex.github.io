43ed6f1e8a5e9beb989972d3428686306b8dae32
parsehex <parsehex@users.noreply.github.com>
2025-10-19 00:22:08 +0000

    deploy: 7e81ead54f5f44399281956c54a03b35c25aa5bc

--- assets/tabler-icons-vue-DvHOHGal.js
+++ assets/tabler-icons-vue-DvHOHGal.js
@@ -1,4 +1,4 @@
-import{h as v}from"./index-BdnFiZeh.js";/**
+import{h as v}from"./index-gXhOfrFq.js";/**
  * @license @tabler/icons-vue v3.35.0 - MIT
  *
  * This source code is licensed under the MIT license.
