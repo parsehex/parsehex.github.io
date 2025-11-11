22117b4af279310497de0408662c60f75c737375
parsehex <1779937+parsehex@users.noreply.github.com>
2025-11-11 15:49:38 -0500

    language filter label

--- src/components/LanguageFilter.vue
+++ src/components/LanguageFilter.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex items-center space-x-2 text-gray-700 dark:text-gray-300">
-		<label> Filter by language: <select :value="selectedLanguage" @change="onLanguageChange($event)"
+		<label> Filter by main language: <select :value="selectedLanguage" @change="onLanguageChange($event)"
 				class="bg-gray-200 dark:bg-gray-700 px-2 py-1 rounded ml-2">
 				<option value="">All</option>
 				<option v-for="lang in allLanguages" :key="lang" :value="lang"> {{ lang }} </option>
