cfa0d60c8f4d3c3265d9f4adee2df734859d2bce
parsehex <parsehex@users.noreply.github.com>
2025-11-16 00:22:19 +0000

    deploy: 75b880074b915c921a3941a295fac88efbdbfdbe

--- assets/tabler-icons-vue-CEMansor.js
+++ assets/tabler-icons-vue-CEMansor.js
@@ -1,4 +1,4 @@
-import{h as v}from"./index-_QQdXh_j.js";/**
+import{h as v}from"./index-Gg_FXj33.js";/**
  * @license @tabler/icons-vue v3.35.0 - MIT
  *
  * This source code is licensed under the MIT license.
