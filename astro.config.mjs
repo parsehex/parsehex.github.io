c793fefa5959d8235baf0e753733cfa43129711d
parsehex <1779937+parsehex@users.noreply.github.com>
2025-11-17 19:06:32 -0500

    fix favicon, attempt env fix again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,19 @@ import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
+console.log('Using username', process.env.VITE_GITHUB_ACTOR);
+
 export default defineConfig({
-	define: {
-		__DEV__: process.env.NODE_ENV === 'development',
-		VITE_GITHUB_ACTOR: process.env.VITE_GITHUB_ACTOR,
-	},
 	integrations: [
 		vue({
 			appEntrypoint: '/src/main.ts',
 		}),
 	],
 	vite: {
+		define: {
+			__DEV__: process.env.NODE_ENV === 'development',
+			VITE_GITHUB_ACTOR: process.env.VITE_GITHUB_ACTOR,
+		},
 		plugins: [tailwindcss(), nodePolyfills()],
 		build: {
 			rollupOptions: {
