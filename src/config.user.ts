1a23cb2eae6ea8f6d617553cc1cfad28a6ced42d
parsehex <1779937+parsehex@users.noreply.github.com>
2025-12-21 12:50:18 -0500

    fix key name

--- src/config.user.ts
+++ src/config.user.ts
@@ -4,9 +4,7 @@ export const userConfig: Partial<Config> = {
 	// https://projectdepot.github.io/SrcGallery/config.html
 	siteTitle: '{username}',
 	profileHeaderCenter: true,
-	footer: {
-		text: '&copy; 2025 Thomas Mays',
-	},
+	footerText: '&copy; 2025 Thomas Mays',
 	links: [
 		{
 			name: 'Mastodon',
