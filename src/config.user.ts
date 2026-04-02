11a8ca5fbf8ff402ff843a0bd3e3ef5e5aae4d27
parsehex <1779937+parsehex@users.noreply.github.com>
2026-04-02 12:28:40 -0400

    Update config.user.ts

--- src/config.user.ts
+++ src/config.user.ts
@@ -8,29 +8,14 @@ export const userConfig: Partial<Config> = {
 	reposFilter: (data) => data.filter(repo => repo.homepage && repo.description),
 	links: [
 		{
-			name: 'Mastodon',
-			icon: 'brand-mastodon',
-			url: 'https://indieweb.social/@parsehex',
-		},
-		{
 			name: 'GitHub',
 			icon: 'brand-github',
 			url: 'https://github.com/parsehex',
 		},
 		{
-			name: 'LinkedIn',
-			icon: 'brand-linkedin',
-			url: 'https://www.linkedin.com/in/thomas-mays-2036b643/',
-		},
-		{
 			name: 'Buy Me A Coffee',
 			icon: 'mug',
 			url: 'https://buymeacoffee.com/parsehex',
-		},
-		{
-			name: 'Patreon',
-			icon: 'brand-patreon',
-			url: 'https://patreon.com/parsehex',
 		},
 		{
 			name: 'Ko-fi',
